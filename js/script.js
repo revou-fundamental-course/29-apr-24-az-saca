c06a61c202d642dfc97e26ccac81efb611a1e0b2
Churun Ain Azkal Azkiya <churunain.ra@unipdu.ac.id>
2024-05-09 12:08:55 +0700

    fixed layouting

--- js/script.js
+++ js/script.js
@@ -24,41 +24,8 @@ function showDivs(n) {
 setInterval(() => plusDiv(1), 10000);
 
 
-// let currentIndex = 3;
-
-// function nextCard(n) {
-//   showCard(n);  
-//   console.log("indexCard " + currentIndex);
-// }
-
-// function showCard(n) {
-//   currentIndex += n;  
-//   let i;
-//   let cardList = document.querySelectorAll(".recom-card .card");
-//   for (i = 0; i < cardList.length; i++) {
-//     cardList[i].style.display = "none";
-//   }
-//   for (i = currentIndex - 3; i < currentIndex + 1; i++) {
-//     if (cardList[i]) {
-//       cardList[i].style.display = "block";     
-//     }
-//   }
-//   if (currentIndex <= 0 || currentIndex === 3) {
-//     document.querySelector("#recom-prev").style.display = "none";
-//   } else {
-//     document.querySelector("#recom-prev").style.display = "block";
-//   }
-//   if (currentIndex >= cardList.length - 1) {
-//     document.querySelector("#recom-next").style.display = "none";
-//   } else {
-//     document.querySelector("#recom-next").style.display = "block";
-//   }
-// }
-
-// showCard(0);
-
 document.addEventListener("DOMContentLoaded", function () {
-  const carousel = document.querySelector(".carousel");
+  const carousel = document.querySelector(".card-container");
   const arrowBtns = document.querySelectorAll(".wrapper i");
   const firstCard = carousel.querySelector(".card");
   const firstCardWidth = firstCard.offsetWidth;
@@ -71,3 +38,33 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 }); 
+
+function validateForm() {
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const subject = document.getElementById("subject").value;
+
+  const nameError = document.getElementById("name-error");
+  const emailError = document.getElementById("email-error");
+  const subjectError = document.getElementById("subject-error");
+
+  nameError.textContent = "";
+  emailError.textContent = "";
+  subjectError.textContent = "";
+
+  let isValid = true;
+
+  if (name === "" || /\d/.test(name)) {
+    isValid = false;
+  }
+
+  if (email === "" || !email.includes("@")) {
+    isValid = false;
+  }
+
+  if (subject === "") {
+    isValid = false;
+  }
+
+  return isValid;
+}
